3e78327c201154b0493ad3ee9b4154ecd08227a8
advaitmenon1106 <advaitmenon1106@gmail.com>
2026-01-31 18:14:26 +0530

    Add paths to each page on the sidebar.

--- frontend/src/Sidebar/Sidebar.jsx
+++ frontend/src/Sidebar/Sidebar.jsx
@@ -1,12 +1,38 @@
+import { ExpenseAnalysis } from '../Expense_Analysis/ExpenseAnalysis'
+import { ExpenseOperations } from '../Expense_Operations/ExpenseOperations'
+import { HomePage } from '../Home/HomePage'
+import { Link } from 'react-router-dom'
 import './Sidebar.css'
 
 export function Sidebar() {
-    const menu_options = ["Home", "Add Expenses", "Analyse", "Assistant"]
+    // const menu_options = ["Home", "Add Expenses", "Analyse", "Assistant"]
+    const menu_options = [
+        {
+            "menu_name": "Home",
+            "linkto": "/home"
+        },
+        {
+            "menu_name": "Add Expenses",
+            "linkto": "/add-expense"
+        },
+        {
+            "menu_name": "Analyse your expenses",
+            "linkto": "/analyse"
+        }
+        // {
+        //     "menu_name": "Home",
+        //     "linkto": <HomePage />
+        // }
+    ]
 
     return (
         <div id="sidebar">
             {menu_options.map((option) => {
-                return <div key={option} className="sidebar-options">{option}</div>
+                return (
+                    <Link to={option['linkto']} key={option['menu_name']}>
+                        <div key={option['menu_name']} className="sidebar-options">{option['menu_name']}</div>
+                    </Link>
+                )
             })}
         </div>
     )
