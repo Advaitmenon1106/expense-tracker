a2449a1112ab783c60cec05cac726baf5758ebe8
advaitmenon1106 <advaitmenon1106@gmail.com>
2026-01-31 18:13:43 +0530

    Add the routing logic to the entry main file.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,43 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { Sidebar } from './Sidebar/Sidebar.jsx'
+import { Sidebar } from './Sidebar/Sidebar'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+import { HomePage } from './Home/HomePage'
+import { ExpenseOperations } from './Expense_Operations/ExpenseOperations'
+import { ExpenseAnalysis } from './Expense_Analysis/ExpenseAnalysis'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <>
+        <Sidebar />
+        <Outlet />
+      </>
+    ),
+    children: [
+      {
+        index: true,
+        path: 'home',
+        element: <HomePage />
+      },
+      {
+        path: 'add-expense',
+        element: <ExpenseOperations />
+      },
+      {
+        path: 'analyse',
+        element: <ExpenseAnalysis />
+      }
+    ]
+  }
+])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Sidebar />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
