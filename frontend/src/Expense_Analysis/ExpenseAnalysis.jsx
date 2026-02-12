987076999eb277cd95241d3dfdefb4eea2f7179a
advaitmenon1106 <advaitmenon1106@gmail.com>
2026-02-13 01:16:36 +0530

    Code the basic menu-based structure for the Expense Analysis.

--- frontend/src/Expense_Analysis/ExpenseAnalysis.jsx
+++ frontend/src/Expense_Analysis/ExpenseAnalysis.jsx
@@ -1,3 +1,19 @@
-export function ExpenseAnalysis() {
+import './ExpenseAnalysis.css'
 
+function MenuTab({ menuDisplay }) {
+    return (
+        <div className="menu-option">
+            {menuDisplay}
+        </div>
+    )
+}
+
+export function ExpenseAnalysis() {
+    return (
+        <div id="all-menus">
+            <MenuTab menuDisplay={"Show All"} />
+            <MenuTab menuDisplay={"Show Some"} />
+            <MenuTab menuDisplay={"Static Analyses"} />
+        </div>
+    )
 }
