317376218d4285b72ef731402edc8c723cb2d1f4
advaitmenon1106 <advaitmenon1106@gmail.com>
2026-02-16 00:33:27 +0530

    Put up a basic state-based frontend rendering component for the show all screen. Also setup basic router endpoints for show all, to harmonize it with the main.jsx file router as well.

--- frontend/src/Expense_Analysis/ExpenseAnalysis.jsx
+++ frontend/src/Expense_Analysis/ExpenseAnalysis.jsx
@@ -1,19 +1,44 @@
+import { Link } from 'react-router-dom'
+import ShowAll from './All_Expenses/ShowAll'
 import './ExpenseAnalysis.css'
+import { useState } from 'react'
 
-function MenuTab({ menuDisplay }) {
+
+function MenuTab({ menuDisplay, onClickFn }) {
     return (
-        <div className="menu-option">
+        <div className="menu-option" onClick={onClickFn}>
             {menuDisplay}
         </div>
     )
 }
 
 export function ExpenseAnalysis() {
+    const [currentView, setCurrentView] = useState(null)
+
+    const handleShowAll = async () => {
+        const response = await fetch(
+            `http://0.0.0.0:8000/retrieve-all-expenses`,
+            {
+                method: "GET"
+            }
+        );
+        const result = await response.json();
+        console.log(result);
+        setCurrentView(<ShowAll data={result} />);
+    }
+
     return (
         <div id="all-menus">
-            <MenuTab menuDisplay={"Show All"} />
+            <Link to={'/analyse/show-all'}>
+                <MenuTab menuDisplay={"Show All"} onClickFn={handleShowAll} />
+            </Link>
+
             <MenuTab menuDisplay={"Apply Filters On Your Data"} />
             <MenuTab menuDisplay={"Static Analyses"} />
+
+            <div>
+                {currentView}
+            </div>
         </div>
     )
 }
