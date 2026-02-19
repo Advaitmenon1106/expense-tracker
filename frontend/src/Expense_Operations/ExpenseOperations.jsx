eb6c458ffbd4e4df440f9c00ab32a73e753bdacf
advaitmenon1106 <advaitmenon1106@gmail.com>
2026-02-20 02:04:26 +0530

    Change the adjoining files as a result of the split.

--- frontend/src/Expense_Operations/ExpenseOperations.jsx
+++ frontend/src/Expense_Operations/ExpenseOperations.jsx
@@ -1,125 +1,26 @@
+import { Link, Outlet } from 'react-router-dom';
 import './ExpenseOperations.css'
-import { useState } from 'react';
+import { MenuTab } from '../Expense_Analysis/ExpenseAnalysis';
 
 
 export function ExpenseOperations() {
-    const expenseFormFields = [
-        {
-            name: "expense_name",
-            label: "Cashflow name",
-            type: "text",
-            required: true
-        },
-        {
-            name: "amount",
-            label: "Amount",
-            type: "number",
-            required: true
-        },
-        {
-            name: "date",
-            label: "Date",
-            type: "date",
-            required: true
-        },
-        {
-            name: "inflow",
-            label: "Is this an inflow?",
-            type: "checkbox",
-            required: true
-        },
-        {
-            name: "tags",
-            label: "Tags",
-            type: "text",
-            required: false
-        },
-        {
-            name: "remarks",
-            label: "Remarks",
-            type: "text",
-            required: false
-        }
-    ];
-
-    const [formData, setFormData] = useState({
-        expense_name: "",
-        amount: "",
-        date: "",
-        inflow: false,
-        tags: "",
-        remarks: ""
-    });
-
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        const params = new URLSearchParams({
-            expense_name: formData.expense_name,
-            amount: Number(formData.amount),
-            date: formData.date,
-            inflow: formData.inflow,
-            tags: formData.tags || "",
-            remarks: formData.remarks || ""
-        });
-
-        const response = await fetch(
-            `http://0.0.0.0:8000/insert-expense?${params.toString()}`,
-            {
-                method: "POST"
-            }
-        );
-
-        if (!response.ok) {
-            console.error("Failed to submit expense");
-            return;
-        }
-
-        const result = await response.json();
-        console.log("Expense inserted:", result);
-    };
-
-    const handleChange = (e) => {
-        const { name, type, value, checked } = e.target;
-
-        setFormData((prev) => ({
-            ...prev,
-            [name]: type === "checkbox" ? checked : value
-        }));
-    };
-
     return (
-        <div className="main-content">
-            <div className="hero-text">Enter an Expense</div>
-            <form id="form" onSubmit={handleSubmit}>
-                {expenseFormFields.map((field) => (
-                    <>
-                        <div className="text-label">
-                            {field.label}
-                        </div>
+        <div className="analysis-page">
 
-                        <input
-                            className="text-label-input"
-                            type={field.type}
-                            name={field.name}
-                            required={field.required}
-                            value={
-                                field.type === "checkbox"
-                                    ? undefined
-                                    : formData[field.name]
-                            }
-                            checked={
-                                field.type === "checkbox"
-                                    ? formData[field.name]
-                                    : undefined
-                            }
-                            onChange={handleChange}
-                        />
-                    </>
-                ))}
-                <button type="submit" style={{ gridColumn: "1 / span 2", marginTop: "2vh" }}>Submit Expense</button>
-            </form>
+            <div id="all-menus">
+                <Link to="add-single">
+                    <MenuTab menuDisplay="Add an expense" />
+                </Link>
+
+                <Link to="statement-upload">
+                    <MenuTab menuDisplay="Upload a bank statement" />
+                </Link>
+            </div>
+
+            <div className="content-area">
+                <Outlet />
+            </div>
+
         </div>
     );
 }
